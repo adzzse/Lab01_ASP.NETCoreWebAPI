3e91861372b80c8d864cab2c203e6b2570f35b71
Adzzse <noik0vshack@gmail.com>
2024-09-12 10:39:10 +0700

    Add project files.

---
+++ BusinessObjects/Category.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects
+{
+    public class Category
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+
+        [StringLength(40)]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+}
